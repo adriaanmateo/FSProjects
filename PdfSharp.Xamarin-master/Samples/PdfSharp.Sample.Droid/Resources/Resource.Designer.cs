da2d4c28abf16107989fc1d847fb634b8b34d74a
amestres <amestreszk@gmail.com>
2019-03-26 08:54:21 +0100

    botón

--- PdfSharp.Xamarin-master/Samples/PdfSharp.Sample.Droid/Resources/Resource.Designer.cs
+++ PdfSharp.Xamarin-master/Samples/PdfSharp.Sample.Droid/Resources/Resource.Designer.cs
@@ -1818,11 +1818,11 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a0027
 			public const int META = 2131361831;
 			
-			// aapt resource value: 0x7f0a0066
-			public const int MyButton = 2131361894;
+			// aapt resource value: 0x7f0a0067
+			public const int MyButton = 2131361895;
 			
-			// aapt resource value: 0x7f0a0068
-			public const int MyWebView = 2131361896;
+			// aapt resource value: 0x7f0a0069
+			public const int MyWebView = 2131361897;
 			
 			// aapt resource value: 0x7f0a0028
 			public const int SHIFT = 2131361832;
@@ -1830,8 +1830,8 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a0029
 			public const int SYM = 2131361833;
 			
-			// aapt resource value: 0x7f0a006c
-			public const int action0 = 2131361900;
+			// aapt resource value: 0x7f0a006d
+			public const int action0 = 2131361901;
 			
 			// aapt resource value: 0x7f0a0057
 			public const int action_bar = 2131361879;
@@ -1854,17 +1854,17 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a0035
 			public const int action_bar_title = 2131361845;
 			
-			// aapt resource value: 0x7f0a0069
-			public const int action_container = 2131361897;
+			// aapt resource value: 0x7f0a006a
+			public const int action_container = 2131361898;
 			
 			// aapt resource value: 0x7f0a0058
 			public const int action_context_bar = 2131361880;
 			
-			// aapt resource value: 0x7f0a0070
-			public const int action_divider = 2131361904;
+			// aapt resource value: 0x7f0a0071
+			public const int action_divider = 2131361905;
 			
-			// aapt resource value: 0x7f0a006a
-			public const int action_image = 2131361898;
+			// aapt resource value: 0x7f0a006b
+			public const int action_image = 2131361899;
 			
 			// aapt resource value: 0x7f0a0002
 			public const int action_menu_divider = 2131361794;
@@ -1881,11 +1881,11 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a0037
 			public const int action_mode_close_button = 2131361847;
 			
-			// aapt resource value: 0x7f0a006b
-			public const int action_text = 2131361899;
+			// aapt resource value: 0x7f0a006c
+			public const int action_text = 2131361900;
 			
-			// aapt resource value: 0x7f0a0079
-			public const int actions = 2131361913;
+			// aapt resource value: 0x7f0a007a
+			public const int actions = 2131361914;
 			
 			// aapt resource value: 0x7f0a0038
 			public const int activity_chooser_view_content = 2131361848;
@@ -1911,20 +1911,23 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a002f
 			public const int bottom = 2131361839;
 			
-			// aapt resource value: 0x7f0a0067
-			public const int button1 = 2131361895;
+			// aapt resource value: 0x7f0a0068
+			public const int button1 = 2131361896;
+			
+			// aapt resource value: 0x7f0a0066
+			public const int button2 = 2131361894;
 			
 			// aapt resource value: 0x7f0a003e
 			public const int buttonPanel = 2131361854;
 			
-			// aapt resource value: 0x7f0a006d
-			public const int cancel_action = 2131361901;
+			// aapt resource value: 0x7f0a006e
+			public const int cancel_action = 2131361902;
 			
 			// aapt resource value: 0x7f0a004e
 			public const int checkbox = 2131361870;
 			
-			// aapt resource value: 0x7f0a0075
-			public const int chronometer = 2131361909;
+			// aapt resource value: 0x7f0a0076
+			public const int chronometer = 2131361910;
 			
 			// aapt resource value: 0x7f0a002b
 			public const int collapseActionView = 2131361835;
@@ -1953,8 +1956,8 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a0022
 			public const int end = 2131361826;
 			
-			// aapt resource value: 0x7f0a007b
-			public const int end_padder = 2131361915;
+			// aapt resource value: 0x7f0a007c
+			public const int end_padder = 2131361916;
 			
 			// aapt resource value: 0x7f0a0039
 			public const int expand_activities_button = 2131361849;
@@ -1974,8 +1977,8 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a003d
 			public const int icon = 2131361853;
 			
-			// aapt resource value: 0x7f0a007a
-			public const int icon_group = 2131361914;
+			// aapt resource value: 0x7f0a007b
+			public const int icon_group = 2131361915;
 			
 			// aapt resource value: 0x7f0a002c
 			public const int ifRoom = 2131361836;
@@ -1983,8 +1986,8 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a003a
 			public const int image = 2131361850;
 			
-			// aapt resource value: 0x7f0a0076
-			public const int info = 2131361910;
+			// aapt resource value: 0x7f0a0077
+			public const int info = 2131361911;
 			
 			// aapt resource value: 0x7f0a0034
 			public const int italic = 2131361844;
@@ -2001,11 +2004,11 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a003c
 			public const int list_item = 2131361852;
 			
-			// aapt resource value: 0x7f0a006f
-			public const int media_actions = 2131361903;
+			// aapt resource value: 0x7f0a0070
+			public const int media_actions = 2131361904;
 			
-			// aapt resource value: 0x7f0a007c
-			public const int message = 2131361916;
+			// aapt resource value: 0x7f0a007d
+			public const int message = 2131361917;
 			
 			// aapt resource value: 0x7f0a0023
 			public const int middle = 2131361827;
@@ -2022,14 +2025,14 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a0010
 			public const int normal = 2131361808;
 			
-			// aapt resource value: 0x7f0a0078
-			public const int notification_background = 2131361912;
+			// aapt resource value: 0x7f0a0079
+			public const int notification_background = 2131361913;
+			
+			// aapt resource value: 0x7f0a0073
+			public const int notification_main_column = 2131361907;
 			
 			// aapt resource value: 0x7f0a0072
-			public const int notification_main_column = 2131361906;
-			
-			// aapt resource value: 0x7f0a0071
-			public const int notification_main_column_container = 2131361905;
+			public const int notification_main_column_container = 2131361906;
 			
 			// aapt resource value: 0x7f0a0040
 			public const int parentPanel = 2131361856;
@@ -2043,11 +2046,11 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a0050
 			public const int radio = 2131361872;
 			
-			// aapt resource value: 0x7f0a0077
-			public const int right_icon = 2131361911;
+			// aapt resource value: 0x7f0a0078
+			public const int right_icon = 2131361912;
 			
-			// aapt resource value: 0x7f0a0073
-			public const int right_side = 2131361907;
+			// aapt resource value: 0x7f0a0074
+			public const int right_side = 2131361908;
 			
 			// aapt resource value: 0x7f0a001b
 			public const int screen = 2131361819;
@@ -2121,8 +2124,8 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a001e
 			public const int src_over = 2131361822;
 			
-			// aapt resource value: 0x7f0a006e
-			public const int status_bar_latest_event_content = 2131361902;
+			// aapt resource value: 0x7f0a006f
+			public const int status_bar_latest_event_content = 2131361903;
 			
 			// aapt resource value: 0x7f0a0051
 			public const int submenuarrow = 2131361873;
@@ -2148,8 +2151,8 @@ namespace PdfSharp.Sample.Droid
 			// aapt resource value: 0x7f0a0044
 			public const int textSpacerNoTitle = 2131361860;
 			
-			// aapt resource value: 0x7f0a0074
-			public const int time = 2131361908;
+			// aapt resource value: 0x7f0a0075
+			public const int time = 2131361909;
 			
 			// aapt resource value: 0x7f0a000e
 			public const int title = 2131361806;
